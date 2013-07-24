7ff83d579eab4aa06bca57e663f49de237369941
Yedhu Krishnan V P <yedhu@outlook.com>
2013-07-24 17:07:42 +0530

    Edit Complete (till) before SAMPLE USERS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
   def new
     @user = User.new
@@ -33,6 +33,10 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def index
+    @users = User.all
   end
 
 
