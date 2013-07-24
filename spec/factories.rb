66f901d730399d0e6235b8a9e91ae2b6f667ae3a
Yedhu Krishnan V P <yedhu@outlook.com>
2013-07-24 17:37:16 +0530

    Edit Complete (till) before PARTIAL REFACTORING

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    name                  "Yedhu Krishnan"
-    email                 "yedhukrishnan@outlook.com"
-    password              "foobar"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
     password_confirmation "foobar"
   end
 end
